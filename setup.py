eb5e286687c9d603df581c176a11d73b8bded69e
Emeteil <94739287+Emeteil@users.noreply.github.com>
2024-11-27 23:12:44 +0700

    fix

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'PySG_gif_support',
-    version = '0.1',
-    description = 'Библиотека для поддержки GIF в PySimpleGUI',
-    author = 'Emeteil',
-    author_email = 'vorobievdima43@gmail.com',
+    name = "PySG_gif_support",
+    version = "0.11",
+    description = "Очень простая библиотека для поддержки GIF в PySimpleGUI",
+    author = "Emeteil",
+    author_email = "vorobievdima43@gmail.com",
     packages = find_packages(),
     install_requires = [
-        'Pillow',
-        'typing',
-        'PySimpleGUI'
+        "Pillow",
+        "typing",
+        "PySimpleGUI"
     ],
-    python_requires = '>=3.6',
+    python_requires = ">=3.6",
+    package_data = {
+        "": ["*.gif"]
+    },
+    include_package_data = True,
 )
