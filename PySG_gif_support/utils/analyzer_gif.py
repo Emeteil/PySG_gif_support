ded75d100fec87f8382d07a6bc9c35ecf0b56777
Emeteil <94739287+Emeteil@users.noreply.github.com>
2024-11-27 23:41:29 +0700

    fix

--- PySG_gif_support/utils/analyzer_gif.py
+++ PySG_gif_support/utils/analyzer_gif.py
@@ -5,7 +5,7 @@ import io, os
 
 def _get_gif_frame_rate(gif_path: Union[str, bytes, os.PathLike]) -> float:
     with Image.open(gif_path) as gif:
-        return 1000 / gif.info['duration']
+        return 1000 / (gif.info['duration'] if gif.info['duration'] else 40)
 
 def _gif_to_base64_frames(gif_path: Union[str, bytes, os.PathLike]) -> Generator[str, None, None]:
     gif = Image.open(gif_path)
