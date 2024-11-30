ba84a65c8582df47a30deabaf733b0e6982e67fe
Emeteil <94739287+Emeteil@users.noreply.github.com>
2024-11-30 22:02:16 +0700

    optimization

--- PySG_gif_support/utils/analyzer_gif.py
+++ PySG_gif_support/utils/analyzer_gif.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageSequence
 from typing import Union, Generator
-import base64
 import io, os
 
 def _get_gif_frame_rate(gif_path: Union[str, bytes, os.PathLike]) -> float:
@@ -18,5 +17,4 @@ def _gif_to_base64_frames(gif_path: Union[str, bytes, os.PathLike]) -> Generator
         frame.save(buffer, format="PNG")
         buffer.seek(0)
         
-        frame_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-        yield frame_base64
+        yield buffer.read()
